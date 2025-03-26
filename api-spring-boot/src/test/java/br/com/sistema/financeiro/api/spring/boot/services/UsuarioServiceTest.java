aac4e7f30f69b23b766caa7e760861c19bcfeaed
willjp04 <sostenes655@gmail.com>
2025-03-26 05:13:37 -0300

    Testes com Junit e Mockito. Testando a criação e busca de usuário.

---
+++ api-spring-boot/src/test/java/br/com/sistema/financeiro/api/spring/boot/services/UsuarioServiceTest.java
@@ -0,0 +1,78 @@
+package br.com.sistema.financeiro.api.spring.boot.services;
+
+import br.com.sistema.financeiro.api.spring.boot.entities.Usuario;
+import br.com.sistema.financeiro.api.spring.boot.repositories.UsuarioRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class UsuarioServiceTest {
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @InjectMocks
+    private UsuarioService usuarioService;
+
+    @Test
+    void testCriarUsuario() {
+
+        Usuario usuario = new Usuario();
+        usuario.setNome("Willian");
+        usuario.setEmail("willian@gmail.com");
+        usuario.setSenha("123456");
+
+        // Simulando o comportamento do save
+        when(usuarioRepository.save(any(Usuario.class))).thenReturn(usuario);
+
+        // Criando o usuário
+        Usuario resultado = usuarioService.criarUsuario(usuario);
+
+        // Verificações do criar usuário
+        assertNotNull(resultado);
+        assertEquals("Willian", resultado.getNome());
+        assertEquals("willian@gmail.com", resultado.getEmail());
+        assertEquals("123456", resultado.getSenha());
+        verify(usuarioRepository, times(1)).save(any(Usuario.class));
+    }
+
+
+    @Test
+    void testBuscarUsuarioPorEmail() {
+        // Criando o usuário de teste
+        Usuario usuario = new Usuario();
+        usuario.setNome("Willian");
+        usuario.setEmail("willian@gmail.com");
+        usuario.setSenha("123456");
+
+        // Simulando o comportamento do findByEmail no repositório
+        when(usuarioRepository.findByEmail("willian@gmail.com")).thenReturn(Optional.of(usuario));
+
+        // Chamando o método buscarUsuarioPorEmail do serviço
+        Optional<Usuario> resultadoOptional = usuarioService.buscarUsuarioPorEmail("willian@gmail.com");
+
+        // Verificando se o resultado não é vazio
+        assertTrue(resultadoOptional.isPresent());
+
+        // Obtendo o usuário dentro do Optional e verificando os dados
+        Usuario usuarioEncontrado = resultadoOptional.get();
+        assertEquals("Willian", usuarioEncontrado.getNome());
+        assertEquals("willian@gmail.com", usuarioEncontrado.getEmail());
+
+        // Verificando se o repositório foi chamado uma vez
+        verify(usuarioRepository, times(1)).findByEmail("willian@gmail.com");
+    }
+
+}

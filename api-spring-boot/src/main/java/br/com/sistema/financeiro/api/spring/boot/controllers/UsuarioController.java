08926551cd4b3ef01057a357e74b64c0828c65c9
willjp04 <sostenes655@gmail.com>
2025-03-26 16:22:02 -0300

    Criação da Classe Controller para usuário e implementação do Endpoint para adicionar usuário.

---
+++ api-spring-boot/src/main/java/br/com/sistema/financeiro/api/spring/boot/controllers/UsuarioController.java
@@ -0,0 +1,26 @@
+package br.com.sistema.financeiro.api.spring.boot.controllers;
+
+
+import br.com.sistema.financeiro.api.spring.boot.entities.Usuario;
+import br.com.sistema.financeiro.api.spring.boot.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UsuarioController {
+
+    @Autowired
+    private  UsuarioService usuarioService;
+
+
+    @PostMapping("/criarUsuario")
+    public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioCriado = usuarioService.criarUsuario(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioCriado);
+    }
+
+
+}

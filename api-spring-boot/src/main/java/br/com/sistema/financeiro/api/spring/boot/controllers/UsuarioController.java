5b3d1fd67c082d4887de5f7ec7094f8bf273f12b
willjp04 <sostenes655@gmail.com>
2025-03-26 16:45:29 -0300

    Criação do endpoint para buscar um usuario por e-mail.

--- api-spring-boot/src/main/java/br/com/sistema/financeiro/api/spring/boot/controllers/UsuarioController.java
+++ api-spring-boot/src/main/java/br/com/sistema/financeiro/api/spring/boot/controllers/UsuarioController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/v1/users")
 public class UsuarioController {
@@ -21,6 +23,15 @@ public class UsuarioController {
         Usuario usuarioCriado = usuarioService.criarUsuario(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioCriado);
     }
+
+
+
+    @GetMapping("/buscarUsuarioPorEmail")
+    public ResponseEntity<Optional<Usuario>> buscarUsuarioPorEmail(@RequestParam String email) {
+        Optional<Usuario> usuario = usuarioService.buscarUsuarioPorEmail(email);
+        return ResponseEntity.status(HttpStatus.OK).body(usuario);
+    }
+
 
 
 }

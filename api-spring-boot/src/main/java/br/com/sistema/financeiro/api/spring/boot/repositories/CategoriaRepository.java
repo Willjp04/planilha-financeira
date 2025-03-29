241c9da3477ced2084639b1c9893d0dce1da7cd2
willjp04 <sostenes655@gmail.com>
2025-03-28 23:40:05 -0300

    Modificação na classe de CatagoriaRepository

--- api-spring-boot/src/main/java/br/com/sistema/financeiro/api/spring/boot/repositories/CategoriaRepository.java
+++ api-spring-boot/src/main/java/br/com/sistema/financeiro/api/spring/boot/repositories/CategoriaRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, UUID> {
-    List<Categoria> findByNomeIgnoreCase(String nome);
+    Optional<Categoria> findByNomeIgnoreCase(String nome);
 
 }
